0b5f84a4fe4dbe5dece55cddc0fd155fcbbea6f1
Abayomi igwubor <igwubor@outlook.com>
2019-07-29 10:25:21 +0100

    Updates

---
+++ precache-manifest.24420e9221718886ea2f45efe96dd1ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f0923be23424e1973d7",
+    "url": "/recipeApp-React-/static/js/runtime~main.b713c482.js"
+  },
+  {
+    "revision": "106e017d105dd3a823eb",
+    "url": "/recipeApp-React-/static/js/main.5ab29022.chunk.js"
+  },
+  {
+    "revision": "df155a8e9073ae43fd1c",
+    "url": "/recipeApp-React-/static/js/2.9e03ac05.chunk.js"
+  },
+  {
+    "revision": "106e017d105dd3a823eb",
+    "url": "/recipeApp-React-/static/css/main.19564e2f.chunk.css"
+  },
+  {
+    "revision": "4261e50a8db96c898df8f83fcaab5d1f",
+    "url": "/recipeApp-React-/index.html"
+  }
+];
